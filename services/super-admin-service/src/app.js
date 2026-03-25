8f2d957e40493505c369a7adfc9914f0b57519e2
Vinayak Thukral <vinayak@sisgain.ae>
2026-03-25 11:20:24 +0530

    Added Super Admin and bulk calls fixes

--- services/super-admin-service/src/app.js
+++ services/super-admin-service/src/app.js
@@ -19,8 +19,8 @@ app.use(hpp());
  * Rate Limiting
  */
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs:60 * 1000,
+  max: 10000,
   message: "Too many requests from this IP, please try again later.",
 });
 
